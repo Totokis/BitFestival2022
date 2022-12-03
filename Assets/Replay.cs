493f448c44c0114c23372dfc2a524a1ff8c7e14e
Totokis <pawelj_janusz@wp.pl>
2022-12-03 17:55:42 +0100

    feat(Game): Add restart on press enter

--- Assets/Replay.cs
+++ Assets/Replay.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,5 +9,15 @@ public class Replay : MonoBehaviour
     public void Clicked()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void Start()
+    {
+        Destroy(FindObjectOfType<PlayerController>()); 
+    }
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
