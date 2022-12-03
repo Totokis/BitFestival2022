9e94474c57124ae75d6b879cf3dda9a37631d6dc
Michał Minikowski <perconey@gmail.com>
2022-12-03 02:23:17 +0100

    sdasd

---
+++ Assets/EnergyController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyController : MonoBehaviour
+{
+    public Single CurrentEnergy = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentEnergy = 50f;
+    
+    }
+
+
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
